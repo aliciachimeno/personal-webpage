5c24ab7652bfb6037b667f351c6cfe2a9996eede
aliciactti24 <aliciachimeno_ext@gencat.cat>
2025-02-17 17:32:39 +0100

    push

--- script.js
+++
@@ -1,7 +0,0 @@
-// In a separate script.js or within a <script> tag at the end of the body
-const menuOpen = document.querySelector('.menu-open');
-const navUl = document.querySelector('nav ul');
-
-menuOpen.addEventListener('click', () => {
-    navUl.classList.toggle('active');
-});
